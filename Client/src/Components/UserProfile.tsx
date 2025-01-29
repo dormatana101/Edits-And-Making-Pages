4393203906a798a76babb5590a0083cd00c448c9
PolyakVladislav <9725860@gmail.com>
2025-01-29 17:05:21 +0200

    UserProfile

--- Client/src/Components/UserProfile.tsx
+++ Client/src/Components/UserProfile.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+// client/src/Components/UserProfile.tsx
+
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import axios from 'axios';
-import '../css/UserProfile.css';
+import styles from '../css/UserProfile.module.css'; // Импортируем CSS Module
 import CONFIG from "../config"; 
 
-
-const UserProfile = () => {
+const UserProfile: React.FC = () => {
   const [userData, setUserData] = useState<any>(null); // נתוני המשתמש
   const [error, setError] = useState<string | null>(null); // הודעות שגיאה
   const [loading, setLoading] = useState(true); // מצב טעינה
@@ -21,8 +22,8 @@ const UserProfile = () => {
   const [loadingMore, setLoadingMore] = useState(false); // מניעת טעינה כפולה
   const [hasMorePosts, setHasMorePosts] = useState(true); // אם יש עוד פוסטים לטעינה
 
-  // פונקציה לשליפת נתוני המשתמש
-  const fetchUserData = async () => {
+  // הפונקציה לשליפת נתוני המשתמש
+  const fetchUserData = useCallback(async () => {
     const token = localStorage.getItem('accessToken');
     if (!token) {
       setError('No token found');
@@ -31,61 +32,61 @@ const UserProfile = () => {
     }
 
     try {
-        // שליפת נתוני המשתמש
-        const response = await axios.get(`${CONFIG.SERVER_URL}/api/users/profile`, {
-          headers: { Authorization: `Bearer ${token}` },
-          params: { userId: localStorage.getItem('userId'), page, limit: 5 },
-        });
-    
-        // עדכון נתוני המשתמש
-        setUserData(response.data);
-        setFormData({
-          username: response.data.user.username,
-          profilePicture: response.data.user.profilePicture,
-        });
-    
-        // שמירת תמונת פרופיל בלוקאלסטורג'
-        if (response.data.user.profilePicture) {
-          localStorage.setItem('profilePicture', response.data.user.profilePicture);
-        }
-    
-        // עדכון הפוסטים
-        setPosts((prevPosts) => {
-            const newPosts = response.data.posts.filter(
-              (post: any) => !prevPosts.some((p: any) => p._id === post._id) // מסנן פוסטים שכבר קיימים
-            );
-            return [...prevPosts, ...newPosts]; // מוסיף את הפוסטים החדשים לפוסטים הקיימים
-          });
-    
-        // בדיקת אם יש עוד פוסטים להציג
-        setHasMorePosts(response.data.hasMorePosts);
-      } catch (error) {
-        setError('Error fetching data');
-      } finally {
-        setLoading(false);
+      // שליפת נתוני המשתמש
+      const response = await axios.get(`${CONFIG.SERVER_URL}/api/users/profile`, {
+        headers: { Authorization: `Bearer ${token}` },
+        params: { userId: localStorage.getItem('userId'), page, limit: 5 },
+      });
+  
+      // עדכון נתוני המשתמש
+      setUserData(response.data);
+      setFormData({
+        username: response.data.user.username,
+        profilePicture: response.data.user.profilePicture,
+      });
+  
+      // שמירת תמונת פרופיל בלוקאלסטורג'
+      if (response.data.user.profilePicture) {
+        localStorage.setItem('profilePicture', response.data.user.profilePicture);
       }
-    };
-    
-    // פונקציה לטעינת פוסטים נוספים בעת גלילה
-    const loadMorePosts = async () => {
-      if (loadingMore || !hasMorePosts) return; // אם כבר טוענים או שאין עוד פוסטים, אל תבצע את הפעולה
-    
-      setLoadingMore(true);
-      setPage((prevPage) => prevPage + 1); // הגדלת הדף
-    
-      try {
-        // טוען את הפוסטים בדף הבא
-        await fetchUserData();
-      } catch (error) {
-        setError('Error fetching more posts');
-      } finally {
-        setLoadingMore(false);
-      }
-    };
-    
-    useEffect(() => {
-      fetchUserData();
-    }, [page]);
+  
+      // עדכון הפוסטים
+      setPosts((prevPosts) => {
+        const newPosts = response.data.posts.filter(
+          (post: any) => !prevPosts.some((p: any) => p._id === post._id) // מסנן פוסטים שכבר קיימים
+        );
+        return [...prevPosts, ...newPosts]; // מוסיף את הפוסטים החדשים לפוסטים הקיימים
+      });
+  
+      // בדיקת אם יש עוד פוסטים להציג
+      setHasMorePosts(response.data.hasMorePosts);
+    } catch (error) {
+      setError('Error fetching data');
+    } finally {
+      setLoading(false);
+    }
+  }, [page]);
+
+  // פונקציה לטעינת פוסטים נוספים בעת גלילה
+  const loadMorePosts = async () => {
+    if (loadingMore || !hasMorePosts) return; // אם כבר טוענים או שאין עוד פוסטים, אל תבצע את הפעולה
+
+    setLoadingMore(true);
+    setPage((prevPage) => prevPage + 1); // הגדלת הדף
+
+    try {
+      // טוען את הפוסטים בדף הבא
+      await fetchUserData();
+    } catch (error) {
+      setError('Error fetching more posts');
+    } finally {
+      setLoadingMore(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, [fetchUserData]);
 
   // טיפול בשינוי ערכים בטופס עריכה
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -100,7 +101,6 @@ const UserProfile = () => {
     }
   };
   
-
   // שמירת שינויים בפרופיל
   const handleSaveChanges = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -152,7 +152,7 @@ const UserProfile = () => {
   };
 
   // שמירת שינויים בפוסט
-  const handleSavePostChanges = async (postId: string,) => {
+  const handleSavePostChanges = async (postId: string) => {
     const token = localStorage.getItem('accessToken');
     if (!token) {
       setError('No token found');
@@ -200,6 +200,8 @@ const UserProfile = () => {
       setError('Error deleting post');
     }
   };
+
+  // טיפול בגלילה לטעינת פוסטים נוספים
   const handleScroll = (event: React.UIEvent<HTMLDivElement, UIEvent>) => {
     const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
 
@@ -208,21 +210,32 @@ const UserProfile = () => {
       loadMorePosts(); // קריאה לפונקציה להטעין פוסטים נוספים
     }
   };
-  
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className={styles.userProfileContainer}>
+        <div className={styles.loading}>
+          <div className={styles.spinner}></div>
+          <p>Loading...</p>
+        </div>
+      </div>
+    );
   }
 
   if (error) {
-    return <div>{error}</div>;
+    return (
+      <div className={styles.userProfileContainer}>
+        <p className={styles.error}>{error}</p>
+      </div>
+    );
   }
 
   return (
-    <div className="user-profile-container">
+    <div className={styles.userProfileContainer}>
       {userData ? (
-        <div className="user-profile-card">
-          <div className="user-info">
+        <div className={styles.userProfileCard} onScroll={handleScroll}>
+          {/* פרטי המשתמש */}
+          <div className={styles.userInfo}>
             <img
               src={
                 selectedFile
@@ -231,88 +244,123 @@ const UserProfile = () => {
                     userData.user.profilePicture ||
                     '/default-profile-picture.png'
               }
-              className="profile-picture"
+              className={styles.profilePicture}
               alt="Profile"
             />
             {editable ? (
-              <div>
+              <div className={styles.editForm}>
                 <input
                   type="text"
                   name="username"
                   value={formData.username}
                   onChange={handleInputChange}
                   placeholder="Enter your username"
+                  className={styles.inputField}
                 />
-                {error && <div className="error-message">{error}</div>}
+                {error && <div className={styles.errorMessage}>{error}</div>}
                 <input
                   type="file"
                   name="profilePicture"
                   onChange={handleFileChange}
                   accept="image/*"
+                  className={styles.inputField}
                 />
-                <button className="save-button" onClick={handleSaveChanges}>Save Changes</button>
-                <button className="cancel-button" onClick={() => setEditable(false)}>Cancel</button>
+                <div className={styles.buttonGroup}>
+                  <button onClick={handleSaveChanges} className={styles.saveButton}>Save Changes</button>
+                  <button onClick={() => setEditable(false)} className={styles.cancelButton}>Cancel</button>
+                </div>
               </div>
             ) : (
-              <div>
-                <h1 className="username">{userData.user.username}</h1>
-                <p className="user-email">Email: {userData.user.email}</p>
-                <button className="edit-button" onClick={() => setEditable(true)}>Edit</button>
+              <div className={styles.viewDetails}>
+                <h1 className={styles.username}>{userData.user.username}</h1>
+                <p className={styles.userEmail}>Email: {userData.user.email}</p>
+                <button onClick={() => setEditable(true)} className={styles.editButton}>Edit</button>
               </div>
             )}
           </div>
-          <div className="user-posts"onScroll={handleScroll}>
-            <h2>My Posts</h2>
-            {userData.posts && userData.posts.length > 0 ? (
-              <ul className="posts-list">
-                {userData.posts.map((post: any) => (
-                  <li key={post._id} className="post-item">
+
+          {/* פוסטים של המשתמש */}
+          <div className={styles.userPosts}>
+            <h2 className={styles.postsHeader}>My Posts</h2>
+            {posts && posts.length > 0 ? (
+              <ul className={styles.postsList}>
+                {posts.map((post) => (
+                  <li key={post._id} className={styles.postItem}>
                     {postEditMode === post._id ? (
                       <div>
-                        <div className="post-edit-field">
+                        <div className={styles.postEditField}>
                           <label>Post Title:</label>
                           <input
                             type="text"
                             value={currentPostTitle}
                             onChange={(e) => setCurrentPostTitle(e.target.value)}
+                            className={styles.postInput}
                           />
                         </div>
-                        <div className="post-edit-field">
+                        <div className={styles.postEditField}>
                           <label>Post Content:</label>
                           <input
                             type="text"
                             value={currentPostContent}
                             onChange={(e) => setCurrentPostContent(e.target.value)}
+                            className={styles.postInput}
                           />
                         </div>
-                        <button onClick={() => handleSavePostChanges(post._id)}>Save</button>
-                        <button onClick={() => setPostEditMode(null)}>Cancel</button>
+                        <div className={styles.buttonGroup}>
+                          <button onClick={() => handleSavePostChanges(post._id)} className={styles.saveButton}>Save</button>
+                          <button onClick={() => setPostEditMode(null)} className={styles.cancelButton}>Cancel</button>
+                        </div>
                       </div>
                     ) : (
                       <div>
-                        <h3>{post.title}</h3>
-                        <p>{post.content}</p>
-                        <button className="edit-button" onClick={() => handlePostEdit(post._id, post.title, post.content)}>Edit</button>
-                        <button className="delete-button" onClick={() => setShowDeleteConfirmation(post._id)}>Delete</button>
+                        <h3 className={styles.postTitle}>{post.title}</h3>
+                        <p className={styles.postContent}>{post.content}</p>
+                        {post.image && (
+                          <img
+                            src={`${CONFIG.SERVER_URL}${post.image}`}
+                            alt="Post"
+                            className={styles.postImage}
+                            loading="lazy"
+                          />
+                        )}
+                        <div className={styles.postActions}>
+                          <button className={styles.editButton} onClick={() => handlePostEdit(post._id, post.title, post.content)}>Edit</button>
+                          <button className={styles.deleteButton} onClick={() => setShowDeleteConfirmation(post._id)}>Delete</button>
+                        </div>
                       </div>
                     )}
-                      {showDeleteConfirmation === post._id && (
-                        <div className="delete-confirmation">
+
+                    {/* וופפוי אישור מחיקה */}
+                    {showDeleteConfirmation === post._id && (
+                      <div className={styles.modalOverlay}>
+                        <div className={styles.modal}>
                           <p>Are you sure you want to delete this post?</p>
-                          <button className="confirm-delete delete-button" onClick={() => handleDeletePost(post._id)}>Delete</button>
-                          <button className="cancel-delete cancel-button" onClick={() => setShowDeleteConfirmation(null)}>Cancel</button>
+                          <div className={styles.modalButtons}>
+                            <button onClick={() => handleDeletePost(post._id)} className={styles.confirmDeleteButton}>Delete</button>
+                            <button onClick={() => setShowDeleteConfirmation(null)} className={styles.cancelDeleteButton}>Cancel</button>
+                          </div>
                         </div>
+                      </div>
                     )}
                   </li>
                 ))}
               </ul>
             ) : (
-              <p>No posts yet</p>
+              <p className={styles.noPosts}>You haven't uploaded any posts yet.</p>
+            )}
+            {loadingMore && (
+              <div className={styles.loadingMore}>
+                <div className={styles.spinner}></div>
+                <p>Loading more posts...</p>
+              </div>
+            )}
+            {!hasMorePosts && (
+              <p className={styles.noMorePosts}>No more posts available.</p>
             )}
           </div>
         </div>
       ) : (
-        <p>No user data available</p>
+        <p>No user data available.</p>
       )}
     </div>
   );
