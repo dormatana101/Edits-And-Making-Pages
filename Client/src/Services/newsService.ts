75b44100a2e29084354cd724d1b685e822453762
dormatana101 <60754331+dormatana101@users.noreply.github.com>
2025-01-29 15:22:05 +0200

    fix

--- Client/src/Services/newsService.ts
+++ Client/src/Services/newsService.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 import CONFIG from "../config";
 export const fetchMedicalNews = async (query: string = "", page: number = 1, pageSize: number = 30) => {
     try {
+      const healthQuery = query ? `${query} AND (health OR medicine OR doctor OR hospital OR disease OR therapy OR wellness OR medical)` : "health";
       const response = await axios.get(CONFIG.NEWS_API_URL, {
         params: {
-          q: query || "health OR medicine OR doctor OR hospital", // Default query
+          q: healthQuery.trim(),
           language: "en",
           pageSize,
           page,
